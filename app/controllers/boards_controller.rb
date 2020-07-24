bb9d17c9161eb58029be6c24ae3d45dd60e93cba
Gavin Liu <du20783@gmail.com>
2020-07-24 16:06:04 +0800

    add show and 404 not_found

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,8 +1,13 @@
 class BoardsController < ApplicationController
+
   def index
     @boards = Board.all
   end
 
+  def show
+    @board = Board.find(params[:id])
+  end
+  
   def new
     @board = Board.new
   end
@@ -13,6 +18,7 @@ class BoardsController < ApplicationController
     if @board.save 
       redirect_to root_path, notice: "新增成功！！"
     else
+      render :new, notice: "errror"
     end
   end
 
