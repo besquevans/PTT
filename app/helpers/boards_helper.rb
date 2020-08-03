353d1320cc51498c45d8e12adccbebacf7a4cc07
Gavin Liu <du20783@gmail.com>
2020-08-03 17:42:06 +0800

    add helper display_bm

--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -1,2 +1,9 @@
 module BoardsHelper
+  def display_bm(board)
+    if board.users.count > 0
+      return board.users.map { |user| user.account }.join(" / ")
+    else
+      return "徵求中"
+    end
+  end
 end
