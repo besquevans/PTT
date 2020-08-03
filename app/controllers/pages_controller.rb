f1c7ea013fccb38b4a5cf3408f71236d83f83583
Gavin Liu <du20783@gmail.com>
2020-08-03 16:40:46 +0800

    add homepage_list helper

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,9 @@
 class PagesController < ApplicationController
   def index
-    @homepage_links = homepage_lists
   end
 
   def about
   end
 
-  def homepage_lists
-    list = {
-      announce: "精華公佈欄",
-      favorite: "我的最愛",
-      class: "分組討論區"
-    }
-  end
+  
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,9 @@
 module PagesHelper
+  def homepage_list
+    {
+      announce: "精華公佈欄",
+      favorite: "我的最愛",
+      class: "分組討論區"
+    }
+  end
 end
