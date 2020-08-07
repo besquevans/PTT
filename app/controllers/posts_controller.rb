07e36e7890302aafcc722a734919cd84a7ac0555
Gavin Liu <du20783@gmail.com>
2020-08-07 11:58:15 +0800

    add edit and update fir posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :find_board, only: [:new, :create]
-    
+  before_action :authenticate_user!, except: [:show]
+
   def show 
     @post = Post.find(params[:id])
   end
@@ -18,12 +19,24 @@ class PostsController < ApplicationController
       render :new
     end
   end
+
+  def edit 
+    @post = current_user.posts.find(params[:id])
+  end
   
+  def update 
+    @post = current_user.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post, notice: '文章更新成功'
+    else
+      render :edit 
+    end
+  end
 
   private 
   
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :content).merge(user: current_user)
   end
 
   def find_board
