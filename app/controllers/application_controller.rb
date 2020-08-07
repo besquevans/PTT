b2c02575e08cda176c4e53dfe776ff8af90c6c59
Gavin Liu <du20783@gmail.com>
2020-08-07 11:57:33 +0800

    add 404 layout false

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   private
   def not_found
-    render file: 'public/404.html', status: 404
+    render file: 'public/404.html', status: 404, layout: false
   end
   
   def current_user
