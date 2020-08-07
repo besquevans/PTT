fbbd7a66e3ce97181db4da5ac98781d0cc9ded07
Gavin Liu <du20783@gmail.com>
2020-08-07 16:39:13 +0800

    add Board and User ralationship

--- app/models/board.rb
+++ app/models/board.rb
@@ -5,4 +5,7 @@ class Board < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :board_masters
   has_many :users, through: :board_masters
+
+  has_many :favorite_boards 
+  has_many :favorited_users, through: :favorite_boards, source: :user
 end
