df2967be3fdc9815e673a8b7c1014c23d71cda20
Gavin Liu <du20783@gmail.com>
2020-08-10 16:33:58 +0800

    import and update Rails ajax for boards favorite

--- app/frontend/controllers/heart_controller.js
+++ app/frontend/controllers/heart_controller.js
@@ -8,7 +8,8 @@
 // </div>
 
 import { Controller } from "stimulus"
-import ax from "axios"
+// import ax from "axios"
+import Rails from "@rails/ujs"
 
 export default class extends Controller {
   static targets = [ "heart" ]
@@ -21,24 +22,43 @@ export default class extends Controller {
     // console.log(this.heartTarget)
     let board_id = this.data.get("id")
 
-    const csrfToken = document.querySelector("meta[name=csrf-token]").content
-    ax.defaults.headers.common['X-CSRF-Token'] = csrfToken
-
-    ax.post(`/boards/${board_id}/favorite.json`)
-      .then((result) => {
-        // console.log(result)
-        console.log(result.data["status"])
-        if (result.data["status"] == true ) {
+    Rails.ajax({
+      url: `/boards/${board_id}/favorite.json`,
+      type: "post",
+      data: "",
+      success: (result) => {
+        console.log(result)
+        if (result["status"] == true ) {
           this.heartTarget.classList.remove("far")
           this.heartTarget.classList.add("fas")
         } else {
           this.heartTarget.classList.remove("far")
           this.heartTarget.classList.add("fas")
         }
-      })
-      .catch(function(err){
+      },
+      error: (err) => {
         console.log(err)
-      })
+      },
+    })
+
+    // const csrfToken = document.querySelector("meta[name=csrf-token]").content
+    // ax.defaults.headers.common['X-CSRF-Token'] = csrfToken
+
+    // ax.post(`/boards/${board_id}/favorite.json`)
+    //   .then((result) => {
+    //     // console.log(result)
+    //     console.log(result.data["status"])
+    //     if (result.data["status"] == true ) {
+    //       this.heartTarget.classList.remove("far")
+    //       this.heartTarget.classList.add("fas")
+    //     } else {
+    //       this.heartTarget.classList.remove("far")
+    //       this.heartTarget.classList.add("fas")
+    //     }
+    //   })
+    //   .catch(function(err){
+    //     console.log(err)
+    //   })
     
   }
 }
