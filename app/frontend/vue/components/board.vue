bf29121f0d684b558d4fcc9ea71fb3791d4429cd
Gavin Liu <du20783@gmail.com>
2020-08-14 16:02:20 +0800

    add board vue

---
+++ app/frontend/vue/components/board.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="board in boards">{{ board }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      boards: [],
+    }
+  }
+}
+</script>
+
