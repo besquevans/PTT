a18b847285f33168746085be26e93935c8382010
Gavin Liu <du20783@gmail.com>
2020-08-17 12:34:51 +0800

    include Pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,24 @@
 class ApplicationController < ActionController::Base
   # rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  include Pundit
 
-
-  helper_method :users_signd_in?, :current_user 
+  helper_method :users_signd_in?, :current_user
 
   private
+
   def not_found
     render file: 'public/404.html', status: 404, layout: false
   end
-  
+
   def current_user
     @usrrent_user ||= User.find_by(id: session[:user_token])
   end
 
   def user_sign_in?
     current_user != nil
-  end 
+  end
 
   def authenticate_user!
-    redirect_to root_path, notice: "請登入會員" if not user_sign_in?
+    redirect_to root_path, notice: '請登入會員' if not user_sign_in?
   end
 end
